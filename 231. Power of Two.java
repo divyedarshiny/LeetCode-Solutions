816b03dfca1c4324ea7c63f9f39e006fcc63d0b3
divyedarshiny <divyedarshiny@gmail.com>
2023-11-18 12:42:56 +0530

    .'

--- 231. Power of Two.java
+++ 231. Power of Two.java
@@ -1,13 +1,6 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        while(n<=0){
-            return false;
-        }
-        for(int i=0;i<=Math.sqrt(n)+1;i++){
-            if(n==Math.pow(2,i)){
-                return true;
-            }
-        }
-        return false;
+        return (n>0) && ((n&(n-1))==0);
     }
 }
+    
